7ee5a0977d455c3bc3b0c84ccfd1defbe8fe89b6
F.H. Oudman <f.h.oudman@student.tue.nl>
2018-06-14 15:09:19 +0200

    SLAM WIP

--- control/control.py
+++ control/control.py
@@ -1,3 +1,9 @@
+from numpy import inf
+from math import sqrt, sin, cos, atan
+from copy import deepcopy
+from statistics import median
+from enum import Enum
+
 '''
 tasks of control thread:
 - receive estimated position from Arduino
@@ -33,22 +39,193 @@ Assumed functions in Arduino:
   Lower gripper arm, open gripper gnum, raise gripper arm
 '''
 
+
+
+'''//------------------------------------------------------------------------//'
+''// temporary placeholders ------------------------------------------------//''
+'//------------------------------------------------------------------------//'''
+# temporary Vision placeholder ----------------------------------------------- #
+def getSurroundings():
+    Num_of_objects = 2
+    object_properties = {'name': 'bottle', 'probability': 0.0, 'position': (0,0), 'stdev_p': 0, 'rotation': 0.0 , 'stdev_r': 0.0}
+    objects_list = [object_properties for _ in range(Num_of_objects)]
+    return objects_list
+
+# temporary Arduino placeholders --------------------------------------------- #
+def getPosition():
+    print('Postion requested')
+    return loc(0,0)
+
+def rotate(angle):
+    print('Rotate {} degrees'.format(angle))
+
+
+
+'''//------------------------------------------------------------------------//'
+''// class definitions -----------------------------------------------------//''
+'//------------------------------------------------------------------------//'''
+# class location, used to store positions on the map ------------------------- #
+class loc:
+    # class variables
+    x = 0.0
+    y = 0.0
+    
+    # constructor
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+# class object, used for objects on the map ---------------------------------- #
+class obj:
+    # class variables
+    sort = Enum("Type", "Cone Bottle Box")
+    prob = 0.0
+    pos = loc(0.0, 0.0)
+    pdev = 0.0
+    rot = 0.0
+    rdev = 0.0
+    
+    # constructor
+    def __init__(self, item):
+        self.sort = item['name']
+        if('probability' in item):
+            self.prob = item['probability']
+        else:
+            self.prob = 1.0
+        self.pos = loc(item['position'][0], item['position'][1]) 
+        if('stdev_p' in item):
+            self.pdev = item['stdev_p']
+        else:
+            self.pdev = 0.0;
+        if('rotation' in item):
+            self.rot = item['rotation']
+            self.rdev = item['stdev_r']
+    
+    # remap object over distance vec, followed by rotation rot
+    def remap(self, vec, rot):
+        self.pos += vec
+        self.rot += rot
+    
+    # remap object as if origin was pos instead of (0,0) and if angle was rot instead of 0
+    def inverseremap(self, pos, angle):
+        self.pos -= pos
+        newx = self.pos.x * cos(angle) + self.pos.y * sin(angle) 
+        newy = self.pos.x * -sin(angle) + self.pos.y * cos(angle)
+        self.pos = loc(newx, newy)
+        self.rot -= angle
+    
+    # calculate distance between object and other object
+    def dist(self, obj):
+        return sqrt((self.pos.x - obj.pos.x)**2 + (self.pos.y - obj.pos.y)**2)
+    
+    # calculate matching error between object and other object
+    def matchError(self, obj):
+        dst = self.dist(obj)
+        magic = 1 + self.pdev + obj.pdev
+        return dst / magic
+
+
+# class map, our virtual map of the surroundings ----------------------------- #
+class plan:
+    # class variables
+    objs = []
+    
+    # constructor
+    def __init__(self):
+        print "new map created, yay"
+    
+    # find match between objects on map and new object. If no match found, return false
+    def findMatch(self, obj):
+        minerr = inf
+        match = False
+        for i in objs:
+            if i.sort == obj.sort:
+                err = matchError(i, obj)
+                if (err < minerr):
+                    minerr = err
+                    match = i
+        return match
+    
+    # add new objects to the map
+    def add(self, obj):
+        self.objs.append(obj)
+
+
+
+'''//------------------------------------------------------------------------//'
+''// other functions -------------------------------------------------------//''
+'//------------------------------------------------------------------------//'''
+# return best fitting rotation for environment env in range (rot-dev) to (rot+dev)
+def imapRotFit(imap, rot, dev, objs):
+    rotp = []
+    for obj in objs:
+        print obj.pos
+        dst = sqrt(obj.pos.x**2 + obj.pos.y**2)
+        if (dst > 0):
+            alp = atan(obj.pos.x / obj.pos.y)
+            match = imap.findMatch({'name': obj.sort, 'position': loc(dst * sin(alp+rot), dst * cos(alp+rot))})
+            if(match == False):
+                print("No match found :(")
+            else:
+                deg = atan(match.pos.x / match.pos.y) - rot
+                print("Match found, {} degrees off")
+                if(abs(deg) < dev):
+                    rotp.append(deg)
+    return median(rotp)
+
+
+
+'''//------------------------------------------------------------------------//'
+''// main code, part I: creating initial map -------------------------------//''
+'//------------------------------------------------------------------------//'''
+# create empty initial map
+imap = plan()
+
 # get environment
+environment = getSurroundings()
+
 # place objects on map
-# rotate by rot degrees
+for i in environment:
+    imap.add(obj(i))
 
-rot = 30
-for i in range(rot, 360, rot):
+# rotation settings
+steps = 12
+step = 360 / steps
+
+# look around 360 degrees
+rotate(step)
+for rot in range(step, 360, step):
     # get environment
+    objs = []
+    environment = getSurroundings()
+    for i in environment:
+        objs.append(obj(i))
+    
     # adjust assumed rotation rot to rot'
+    rotp = imapRotFit(imap, rot, step/2, objs)
+    
+    # remap position of observed objects to coordinate system of map
+    # TODO
+    
     # update map
-    # rotate by rot degrees
+    # TODO
+    
+    # rotate by step degrees, adjust for rotp
+    rotate(step + (rot-rotp))
 
+
+
+'''//------------------------------------------------------------------------//'
+''// main code, part II: remapping to final map with more features, yay ----//''
+'//------------------------------------------------------------------------//'''
 # remap items on map to a new rectangular map using the given corner bottle order
+# TODO
 
 # make a list of cone statuses
+# TODO
 
 # make an empty list of boxes held by the robot
+# TODO
 
 '''
 At this point we have a nice map containing the following:
@@ -66,6 +243,11 @@ Also, we have a list of boxes held by the robot, having per box
 - delivery address
 '''
 
+
+
+'''//------------------------------------------------------------------------//'
+''// main code, part III: driving around, picking up bitches and boxes -----//''
+'//------------------------------------------------------------------------//'''
 # while there are cones with undelivered boxes next to it
     # if gripper available
         # move to nearest delivery address OR nearest cone with undelivered boxes
@@ -75,5 +257,10 @@ Also, we have a list of boxes held by the robot, having per box
         # drop box
     # if at cone with undelivered boxes
         # pickup box        
-        
-# done
+
+
+
+'''//------------------------------------------------------------------------//'
+''// main code, part IV: done ----------------------------------------------//''
+'//------------------------------------------------------------------------//'''
+print("Done. Yay. Beer?")
