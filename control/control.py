8096444f9b8a6dbe5ddd9a341c4e0015f75282a4
F.H. Oudman <f.h.oudman@student.tue.nl>
2018-06-21 23:31:52 +0200

    WIP: stage I testing with debug map

--- control/control.py
+++ control/control.py
@@ -16,12 +16,19 @@ tasks of control thread:
 - keep track of status of persons and boxes
 
 Assumed functions in visual thread:
-- list[object, probability, position, stdev, rotation, stdev] getSurroundings()
+- getSurroundings()
   After calling this function, the visual thread takes a few pictures of the
-  environment and returns a list of potential seen objects. Each object has a 
-  probability of being that specific object, an estimated position and standard
-  deviation and an estimated rotation and standard deviation. These last two
-  parameters are only useful for boxes, not for cones and bottles.
+  environment and returns a list of potential seen objects. Objects have the
+  following format:
+    minBottle = {'name': 'bottle', 'position': (0.0, 0.0), 'color': 'yellow'}
+    maxBottle = {'name': 'bottle', 'probability': 1.0, 'position': (0.0, 0.0), 'stdev_p': 0.0, 'color': 'yellow'}
+    minCone = {'name': 'cone', 'position': (0.0, 0.0)}
+    maxCone = {'name': 'cone', 'probability': 1.0, 'position': (0.0, 0.0), 'stdev_p': 0.0}
+    minBox = {'name': 'box', 'position': (0.0, 0.0), 'rot': 0.0}
+    maxBox = {'name': 'box', 'probability': 1.0, 'position': (0.0, 0.0), 'stdev_p': 0.0, 'rot': 0.0}
+  All objects have a parameter name, an optional parameter probability, a
+  parameter position, and an optional parameter stdev_p. All bottle objects
+  have a parameter color. All box objects have a parameter rot.
 
 Assumed functions in Arduino:
 - position getPosition()
@@ -41,6 +48,9 @@ Assumed functions in Arduino:
 '''
 
 
+DEBUG = True
+
+
 
 '''//------------------------------------------------------------------------//'
 ''// class definitions -----------------------------------------------------//''
@@ -48,13 +58,13 @@ Assumed functions in Arduino:
 # class location, used to store positions on the map ------------------------- #
 class Loc(object):
     # class variables
-    x = 0.0
-    y = 0.0
+    # x = 0.0
+    # y = 0.0
     
     # constructor
     def __init__(self, x, y):
-        self.x = x
-        self.y = y
+        self.x = float(x)
+        self.y = float(y)
     
     # string generator
     def __str__(self):
@@ -64,13 +74,15 @@ class Loc(object):
 # class object, used for objects on the map ---------------------------------- #
 class Obj(object):
     # class variables
-    prob = 1.0
-    pos = 0.0
-    pdev = 0.0
+    # prob = 1.0
+    # views = 0
+    # pos = Loc(0.0, 0.0)
+    # pdev = 0.0
     
     # constructor
     def __init__(self, prob, pos, pdev):
         self.prob = prob
+        self.views = 1
         self.pos = pos
         self.pdev = pdev
     
@@ -95,6 +107,23 @@ class Obj(object):
     def dist(self, obj):
         return sqrt((self.pos.x - obj.pos.x)**2 + (self.pos.y - obj.pos.y)**2)
     
+    # calculate angle between object and other object
+    def angle(self, obj):
+        dx = float(obj.pos.x - self.pos.x)
+        dy = float(obj.pos.y - self.pos.y)
+        if dx == 0 and dy == 0:
+            return 0.0
+        elif dy == 0:
+            return pi * (dx > 0)
+        elif dx == 0:
+            return pi * (dy > 0) - pi/2
+        elif dy > 0:
+            return atan(dx / dy)
+        elif dx > 0:
+            return atan(dx / dy) + pi
+        else:
+            return atan(dx / dy) - pi
+    
     # calculate matching error between object and other object
     def matchError(self, obj):
         dst = self.dist(obj)
@@ -105,7 +134,7 @@ class Obj(object):
 # class bottle, used to represent the corner bottles of the field -------------#
 class Bottle(Obj):
     # class variables
-    color = Enum("Color", "yellow orange blue purple")
+    # color = Enum("Color", "yellow orange blue purple")
     
     # constructor
     def __init__(self, prob, pos, pdev, color):
@@ -124,8 +153,8 @@ class Bottle(Obj):
 # class cone, used to represent the cones in the field ------------------------#
 class Cone(Obj):
     # class variables
-    name = None
-    boxesDone = False
+    # name = None
+    # boxesDone = False
     
     # constructor
     def __init__(self, prob, pos, pdev):
@@ -155,10 +184,10 @@ class Cone(Obj):
 # class box, used to represent the boxes in the field -------------------------#
 class Box(Obj):
     # class variables
-    rot = 0.0
-    origin = None
-    dest = None
-    delivered = False
+    # rot = 0.0
+    # origin = None
+    # dest = None
+    # delivered = False
     
     # constructor
     def __init__(self, prob, pos, pdev, rot):
@@ -194,39 +223,86 @@ class Box(Obj):
 # class map, our virtual map of the surroundings ----------------------------- #
 class Map(object):
     # class variables
-    objs = []
+    # objs = []
     
     # constructor
     def __init__(self):
-        objs = []
+        self.objs = []
+    
+    # getters/setters
+    def setSize(self, x, y):
+        self.width = x
+        self.length = y
+    
+    # add new objects to the map
+    def add(self, obj):
+        self.objs.append(obj)
     
     # find match between objects on map and new object. If no match found, return false
     def findMatch(self, obj):
         minerr = inf
         match = False
         for i in objs:
-            if i.sort == obj.sort:
+            if type(i) == type(obj):
                 err = matchError(i, obj)
                 if (err < minerr):
                     minerr = err
                     match = i
         return match
     
-    # add new objects to the map
-    def add(self, obj):
-        self.objs.append(obj)
+    # Fit a second map to the first map, return combined map. Starting point of
+    # alignment is position (0,0) on map B mapped on map A, rotated by rot
+    # radians. Maximum position deviation is fixed to pdev, maximum rotation
+    # deviation is fixed to rdev
+    def combine(self, mapb, pos, pdev, rot, rdev):
+        print("TODO")
     
     # debug print: print all objects
-    def debugPrint(self):
+    def debugPrint(self, nice=False, xx=None, xy=None):
         for obj in self.objs:
-            print(obj)
+            print("{}\t{}".format(self.objs.index(obj), obj))
+        if nice:
+            xscale = 16
+            yscale = 40
+            xmin = xmax = 0.0
+            ymin = ymax = 0.0
+            posx = []
+            posy = []
+            for obj in self.objs:
+                x = obj.pos.x
+                y = obj.pos.y
+                xmin = min(xmin, x)
+                xmax = max(xmax, x)
+                ymin = min(ymin, y)
+                ymax = max(ymax, y)
+                posx.append(x)
+                posy.append(y)
+            for i in range(0, len(posx)):
+                posx[i] = round((posx[i]-xmin)/xscale)
+                posy[i] = round((posy[i]-ymin)/yscale)
+            for y in range(int(round((ymax-ymin)/yscale)), -1, -1):
+                line = "\t"
+                for x in range(0, int(round((xmax-xmin)/xscale))+1):
+                    match = False
+                    for i in range(len(posx)):
+                        if posx[i] == x and posy[i] == y:
+                            idx = i
+                            match = True
+                            break
+                    if xx != None and xy != None and round((xx-xmin)/xscale) == x and round((xy-ymin)/yscale) == y:
+                        line += 'X'
+                    elif match:
+                        line += str(idx)
+                    else:
+                        line += '.'
+                print(line)
 
 
 
 '''//------------------------------------------------------------------------//'
-''// placeholders & debug map ----------------------------------------------//''
+''// test map & vision + Arduino placeholders ------------------------------//''
 '//------------------------------------------------------------------------//'''
-# create debug map
+# create test map
 debugmap = Map()
 xmin = 0.0
 xmax = random.randint(500, 1000)
@@ -236,50 +312,70 @@ debugmap.add(Bottle(1.0, Loc(xmin, ymin), 0.0, 'yellow'))
 debugmap.add(Bottle(1.0, Loc(xmin, ymax), 0.0, 'orange'))
 debugmap.add(Bottle(1.0, Loc(xmax, ymax), 0.0, 'blue'))
 debugmap.add(Bottle(1.0, Loc(xmax, ymin), 0.0, 'purple'))
-
-names = ['Inaki', 'Oudman', 'Amin', 'Random']
-
-for i in range(4):
-    x = random.randint(100, xmax-100)
-    y = random.randint(100, ymax-100)
+names = ['Inaki', 'Oudman', 'Amin']
+boxes = 0
+for i in range(3):
+    x = random.randint(50, xmax-50)
+    y = random.randint(50, ymax-50)
     cone = Cone(1.0, Loc(x, y), 0.0)
     cone.setName(names[i])
     debugmap.add(cone)
-    n = random.randint(0, 2)
+    n = random.randint(1, 3)
     for j in range(n):
         xp = random.randint(x-25, x+25)
         yp = random.randint(y-25, y+25)
         rot = 2*pi*random.rand()
-        dest = random.randint(0,2)
-        box = Box(1.0, Loc(xp, yp), 0.0, rot)
-        box.setData(names[i], names[dest])
-        debugmap.add(box)
+        dest = random.randint(0,3)
+        if dest != i and boxes < 3:
+            box = Box(1.0, Loc(xp, yp), 0.0, rot)
+            box.setData(names[i], names[dest])
+            debugmap.add(box)
+            boxes += 1
+robotx = random.randint(50, xmax-50)
+roboty = random.randint(50, ymax-50)
+robotrot = 2*pi*random.rand()
+robot = Obj(1.0, Loc(robotx, roboty), 0.0)
+if DEBUG:
+    print("Debug map created:")
+    debugmap.debugPrint(True, robotx, roboty)
+    print("Robot placed at position ({}, {}) with rotation {:5.3}\n\n".format(robotx, roboty, robotrot))
 
-print("Debug map created:")
-debugmap.debugPrint()
-sleep(10)
-print("Continue...")
-
-
-
-# temporary Vision placeholder ----------------------------------------------- #
+# vision thread placeholder, uses debug map----------------------------------- #
 def getSurroundings():
-    minBottle = {'name': 'bottle', 'position': (0.0), 'color': 'yellow'}
-    maxBottle = {'name': 'bottle', 'probability': 1.0, 'position': (0.0), 'stdev_p': 0.0, 'color': 'yellow'}
-    minCone = {'name': 'cone', 'position': (0.0)}
-    maxCone = {'name': 'cone', 'probability': 1.0, 'position': (0.0), 'stdev_p': 0.0}
-    minBox = {'name': 'box', 'position': (0.0), 'rot': 0.0}
-    maxBox = {'name': 'box', 'probability': 1.0, 'position': (0.0), 'stdev_p': 0.0, 'rot': 0.0}
-    objects_list = [minBottle, maxBottle, minCone, maxCone, minBox, maxBox]
+    print("getSurroundings()")
+    objects_list = []
+    for obj in debugmap.objs:
+        dist = robot.dist(obj)
+        angle = (robot.angle(obj) - robotrot + pi) % (2*pi) - pi
+        if angle > (-pi/3) and angle < (pi/3):
+            if DEBUG:
+                print("Object in sight, dist: {:5.1f}, angle: {:6.2f}".format(dist, angle))
+            posx = dist * sin(angle)
+            posy = dist * cos(angle)
+            if isinstance(obj, Bottle):
+                if obj.pos.x == 0.0 and obj.pos.y == 0:
+                    color = 'yellow'
+                elif obj.pos.x == 0.0:
+                    color = 'orange'
+                elif obj.pos.y == 0.0:
+                    color = 'purple'
+                else:
+                    color = 'blue'
+                objects_list.append({'name': 'bottle', 'probability': 1.0, 'position': (posx,posy), 'stdev_p': 0.0, 'color': color})
+            elif isinstance(obj, Cone):
+                objects_list.append({'name': 'cone', 'probability': 1.0, 'position': (posx,posy), 'stdev_p': 0.0})
     return objects_list
 
-# temporary Arduino placeholders --------------------------------------------- #
+# Arduino placeholder -------------------------------------------------------- #
 def getPosition():
     print('Postion requested')
     return Loc(0,0)
 
+# Arduino placeholder -------------------------------------------------------- #
 def rotate(angle):
-    print('Rotate {} degrees'.format(angle))
+    print('Rotate by {} radians\n\n'.format(angle))
+    global robotrot
+    robotrot += angle
 
 
 
@@ -314,10 +410,10 @@ def imapRotFit(imap, rot, dev, objs):
     rotp = []
     for obj in objs:
         print obj.pos
-        dst = sqrt(obj.pos.x**2 + obj.pos.y**2)
+        dst = obj.dist(Obj(0.0, Loc(0.0, 0.0), 0.0))
         if (dst > 0):
             alp = atan(obj.pos.x / obj.pos.y)
-            match = imap.findMatch({'name': obj.sort, 'position': Loc(dst * sin(alp+rot), dst * cos(alp+rot))})
+            match = imap.findMatch(Obj(0.0, Loc(dst * sin(alp+rot), dst * cos(alp+rot)), 0.0))
             if(match == False):
                 print("No match found :(")
             else:
@@ -342,18 +438,19 @@ environment = getSurroundings()
 for i in environment:
     imap.add(parse(i))
 
-print("debug stuff:")
-imap.debugPrint()
-sleep(10)
-print("continueing")
+if DEBUG:
+    print("Initial map created:")
+    imap.debugPrint(True, 0, 0)
+    print("Robot at (0.0, 0.0) with rotation 0.0\n\n")
 
 # rotation settings
 steps = 12
-step = 360 / steps
+step = 2 * pi / steps
 
 # look around 360 degrees
 rotate(step)
-for rot in range(step, 360, step):
+rot = step
+while (rot < 2 * pi):
     # get environment
     objs = []
     environment = getSurroundings()
@@ -367,12 +464,18 @@ for rot in range(step, 360, step):
     for obj in objs:
         obj.inverseremap(0.0, rotp)
     
-    
     # update map
     # TODO
     
     # rotate by step degrees, adjust for rotp
     rotate(step + (rot-rotp))
+    rot += step
+    
+    if DEBUG:
+        print("Updated map")
+        imap.debugPrint(True, 0, 0)
+        print("Robot at (0.0, 0.0) with rotation {}\n\n".format(rotp))
+        sleep(10)
 
 
 
